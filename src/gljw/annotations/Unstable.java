d7fe53394d0f48add1f6b2e92edec76c241f33c2
Miracle-Masterpiece <mrmiraclemasterpiece@gmail.com>
2024-04-02 12:05:04 +0300

    add copyright comment

--- src/gljw/annotations/Unstable.java
+++ src/gljw/annotations/Unstable.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2024 Miracle-Masterpiсe <mrmiraclemasterpiece@gmail.com or https://t.me/MiracleMasterpiece>. All rights reserved.
+ * Use is subject to license terms.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
 package gljw.annotations;
 
 /**
