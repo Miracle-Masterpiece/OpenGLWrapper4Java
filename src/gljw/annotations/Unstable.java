bd67d5a344313640c72869372806d0c58806a728
Miracle-Masterpiece <mrmiraclemasterpiece@gmail.com>
2024-04-03 14:56:30 +0300

    docfix

--- src/gljw/annotations/Unstable.java
+++ src/gljw/annotations/Unstable.java
@@ -13,7 +13,7 @@
 package gljw.annotations;
 
 /**
- *This annotation signs the code that can crash the Java virtual machine in certain situations.
+ * This annotation signs the code that can crash the Java virtual machine in certain situations.
  * */
 public @interface Unstable {
 	String value();
